2623d270e11aa6f541f2da977d1939c0acd69312
Ynng <44907675+Ynng@users.noreply.github.com>
2020-04-30 12:51:42 -0400

    nah, I wrote this
    Former-commit-id: cbceef75efc89faff11435fadfebf8f4612447fd

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,3 @@
-/* ----------------------------------
-
-Name: custom.js
-Author: Fourtabs
-Website: https://www.fourtabs.com
-Exclusively available here: https://themeforest.net/user/fourtabsthemes
-
-------------------------------------- */
-
-
 /*====================================================
 
 
