6d996c163283e9a0290f25a27a6a021bafeb8ae2
Ynng <44907675+Ynng@users.noreply.github.com>
2020-05-17 04:51:14 -0400

    moved/renamed javascripts files, finished team page

--- js/custom/gallery.js
+++ js/custom/gallery.js
@@ -4,13 +4,6 @@
 
 ========================*/	
 
-var $masonryItemContainer = $('.masonry-item-container').masonry({
-  itemSelector: '.masonry-item',
-  percentPosition: true,
-  columnWidth: '.masonry-sizer',
-  gutter: 5
-});
-
 const observer = lozad('.lozad', {
   load: function (el) {
     el.src = el.getAttribute('data-src');
