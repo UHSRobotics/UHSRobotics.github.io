ca6c7d9a83e3fde14cee4889c7765e8a8ce46470
Ynng <44907675+Ynng@users.noreply.github.com>
2020-05-18 00:32:05 -0400

    improved lozad script

--- js/custom/lozad.js
+++ js/custom/lozad.js
@@ -1,2 +1,10 @@
-const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+
+const observer = lozad('.lozad', {
+  load: function (el) {
+    el.src = el.getAttribute('data-src');
+    el.classList.add("loaded");
+    // Custom implementation to load an element
+    // e.g. el.src = el.getAttribute('data-src');
+  }
+});
 observer.observe();
